fd3b3f10ebefe51fea87c53b720f1e870f9857b4
alking <aijingsun6@gmail.com>
2018-10-23 00:21:52 +0800

    first commit

---
+++ Properties4Net/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Text;
+namespace Properties4Net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string root = Environment.CurrentDirectory;
+            Console.WriteLine(root);
+
+            string fooPath = Path.Join(root,"Data/Foo.properties");
+
+            Properties properties = new Properties(fooPath);
+
+            Console.WriteLine(properties);
+
+            Console.WriteLine(properties["language"]);
+
+        }
+    }
+}
